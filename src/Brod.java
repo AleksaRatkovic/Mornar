926abbea9809da071d2d11eb0739a2fdf4b59a5e
aleksa.ratkovic <aleksa.ratkovic@cbs.co.rs>
2023-02-02 02:22:48 +0100

    adding some methods to class brod

--- src/Brod.java
+++ src/Brod.java
@@ -1,2 +1,50 @@
-public class Brod {
+public abstract class Brod {
+    protected String naziv;
+    protected Mornar[] mornari;
+    int kap, br =0;
+    Mornar kapetan;
+
+    public Brod(String n, int k, Mornar m) {
+        this.naziv = n;
+        this.mornari = new Mornar[kap];
+    }
+
+    public void dodajMornara(Mornar m) {
+        if(br != kap) mornari[br++] = m;
+    }
+
+   public Mornar dohvMornaraPrekoIndeksa(int i) throws GIndeks(){
+        if(i< 0 || i > kap) throw new GIndeks();
+        return mornari[i];
+    }
+
+    public int NajgoriMornar() {
+        int tmp = 101;
+        int indeks=0;
+        for(int i =0; i< br; i++){
+            if(mornari[i].dohvKvalitet() < tmp)
+            {
+                tmp = mornari[i].dohvKvalitet();
+                indeks = i;
+            }
+        }
+        if(tmp ==101) return -1;
+        else return indeks;
+    }
+
+    int dohvTrenutniBrojMornara()
+    {
+        return br;
+    }
+
+    void ukloniMornare()
+    {
+        for(int i = 0; i<br; i++) mornari[i] = null;
+        br = 0;
+    }
+
+
+
+
+
 }

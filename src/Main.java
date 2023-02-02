b954dbb012a1051c4d125d2968bb9d09468c463b
aleksa.ratkovic <aleksa.ratkovic@cbs.co.rs>
2023-02-02 01:41:39 +0100

    zavrsena prva klasa, krecemo na drugu

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] Args)
+    {
+        Mornar m = new Mornar(52,"Djole");
+
+        System.out.println(m);
+    }
+}
---
+++ src/Mornar.java
@@ -0,0 +1,22 @@
+public class Mornar {
+    private int kvalitet;
+    private String ime;
+
+    public Mornar(int k, String ime)
+    {
+        if(k>100) this.kvalitet = 100;
+        else if(k<0) this.kvalitet = 0;
+        else this.kvalitet = k;
+
+        this.ime = ime;
+    }
+
+    int dohvKvalitet() {return this.kvalitet;}
+    String dohvIme() {return this.ime;}
+
+    @Override
+    public String toString() {
+        return "M_" + this.ime + " : " + this.kvalitet;
+    }
+
+}
